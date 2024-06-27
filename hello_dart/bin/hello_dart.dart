97905bca0796919fa4be4bd0816c73fcadfd090d
hemanthgopisetty <hemanth.508136@gmail.com>
2024-06-27 12:42:50 +0530

    About this project Details

--- hello_dart/bin/hello_dart.dart
+++ hello_dart/bin/hello_dart.dart
@@ -3,3 +3,10 @@ import 'package:hello_dart/hello_dart.dart' as hello_dart;
 void main(List<String> arguments) {
   print('Hello world: ${hello_dart.calculate()}!');
 }
+
+
+/**
+ * Executable code will be in this folder
+ * 
+ * In this file we are importing a function calculate from a package which was in the libray folder
+ */
