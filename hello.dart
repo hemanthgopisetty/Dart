53d9b7bcae7b507b787593e672cce179d61f51af
hemanthgopisetty <hemanth.508136@gmail.com>
2024-06-27 12:22:55 +0530

    Hello dart Program

--- hello.dart
+++ hello.dart
@@ -1,3 +1,13 @@
 void main(List<String> args) {
   print("Hello world");
 }
+/**
+ * main is the name of the function where all dart programs will start
+ * it act's as a starting point for the dart program
+ * 
+ * inside that function we have printing fucntion that is printing hello world on the terminal 
+ * 
+ * manually running the dart progrgam by code 
+ * dart run hello.dart
+ * 
+ */
